b2abbd2e293ed37c2120824d89abe3cd3df30cc6
NLetenneur <n.letenneur@gmail.com>
2024-04-11 14:15:03 +0200

    ex 05 à 09

--- ES6/mouli_es6/exercices/06_composition.js
+++ ES6/mouli_es6/exercices/06_composition.js
@@ -1,42 +1,48 @@
 /**
  * utiliser l'opérateur de composition ... afin de fusionner 2 tableaux passés en paramètres
- * 
+ *
  * ex: [1, 2, 3], [4, 5, 6] => [1, 2, 3, 4, 5, 6]
- * 
- * contrainte: 
+ *
+ * contrainte:
  *  - ne pas utiliser la méthode concat, map, merge, push
  *  - for, foreach, while, do while sont interdits
  */
 
-const concat
+const concat = (tab1, tab2) => {
+  return (newTab = [...tab1, ...tab2]);
+};
 
 /**
  * utiliser l'opérateur de composition ... afin d'ajouter un élément à un tableau
- * 
+ *
  * ex: [1, 2, 3], 4 => [1, 2, 3, 4]
- * 
+ *
  * contrainte:
  * - ne pas utiliser la méthode push
  */
 
-const push
+const push = (array, value) => {
+  return (array = [...array, value]);
+};
 /**
  * utiliser l'opérateur de composition ... afin de fusionner 2 objets passés en paramètres
- * 
+ *
  * ex: {a: 1, b: 2}, {c: 3, d: 4} => {a: 1, b: 2, c: 3, d: 4}
  */
 
-const merge
+const merge = (obj1, obj2) => {
+  return (obj3 = { ...obj1, ...obj2 });
+};
 /**
  * utiliser l'opérateur de composition ... afin de modifier la propriété name de l'objet
- * 
+ *
  * ex: {name: 'toto'}, 'titi' => {name: 'titi'}
- * 
+ *
  * contrainte:
  *  - interdiction d'utiliser l'opérateur d'affectation "="
  */
 
-const setName
+const setName = (obj, name) => ({ ...obj, name });
 
 // astuce: {...obj} crée une copie de l'objet, c'est un des principes de l'immutabilité et évite les problèmes de référence
-module.exports = {concat, push, merge, setName}
+module.exports = { concat, push, merge, setName };
